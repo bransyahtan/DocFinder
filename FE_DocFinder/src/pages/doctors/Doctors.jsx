7373a0c33ed2633a5effb5ece7c293b77a284e87
bransyahtan <bransyahhhh@gmail.com>
2024-02-09 13:15:53 +0700

    service and doctor page

--- FE_DocFinder/src/pages/doctors/Doctors.jsx
+++ FE_DocFinder/src/pages/doctors/Doctors.jsx
@@ -1,3 +1,38 @@
+import { doctors } from "../../assets/data/doctors";
+import { DoctorCard } from "../../components/Doctors/DoctorCard";
+
 export const Doctors = () => {
-  return <div>Doctors</div>;
+  return (
+    <>
+      <section>
+        <div className="container text-center">
+          <h2 className="heading">Meet Our Best Team of Doctors</h2>
+          <div
+            className="max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-xl flex items-center
+          justify-between"
+          >
+            <input
+              type="search"
+              className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer
+            placeholder:text-textColor"
+              placeholder="Search Doctor"
+            />
+            <button className="btn mt-0 rounded-[0px] rounded-r-md">
+              Search
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 lg:gap-7 mt-7 lg:mt-14">
+            {doctors.map((doctor) => (
+              <DoctorCard key={doctor.id} doctor={doctor} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
