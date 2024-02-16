dd5c658f6d3100a06a49d0c9c41713ebc4155811
bransyahtan <bransyahhhh@gmail.com>
2024-02-16 23:38:22 +0700

    doctor page create doctor about

--- FE_DocFinder/src/pages/doctors/DoctorAbout.jsx
+++ FE_DocFinder/src/pages/doctors/DoctorAbout.jsx
@@ -1,3 +1,5 @@
+import { formatDate } from "../../utils/formatDate";
+
 export const DoctorAbout = () => {
   return (
     <div>
@@ -19,6 +21,71 @@ export const DoctorAbout = () => {
           me to deliver compassionate and proficient treatment to individuals in
           need.
         </p>
+      </div>
+
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Education
+        </h3>
+        <ul className="pt-4 md:p-5">
+          <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+            <div>
+              <span className="text-irisBlueColor text-[16px] leading-6 font-semibold">
+                {formatDate("07-23-2005")} - {formatDate("07-23-2008")}
+              </span>
+              <p className="text-[18px] leading-6 font-medium text-textColor">
+                PhD in Surgeon
+              </p>
+            </div>
+            <p className="text-[16px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York
+            </p>
+          </li>
+          <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+            <div>
+              <span className="text-irisBlueColor text-[16px] leading-6 font-semibold">
+                {formatDate("07-23-2008")} - {formatDate("07-23-2012")}
+              </span>
+              <p className="text-[18px] leading-6 font-medium text-textColor">
+                Prof in War Wound Surgeon
+              </p>
+            </div>
+            <p className="text-[16px] leading-5 font-medium text-textColor">
+              Mikaela Yoshikawa Hospital, Tokyo
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mt-9">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Experiences
+        </h3>
+
+        <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+          <li className="p-4 rounded-xl bg-[#dcedc1]">
+            <span className="text-irisBlueColor text-[16px] leading-6 font-semibold ">
+              {formatDate("01-23-2013")} - {formatDate("05-23-2018")}
+            </span>
+            <p className="text-[18px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
+            <p className="text-[16px] leading-5 font-medium text-textColor">
+              Al-Ahly Hospital, Saudi Arabia
+            </p>
+          </li>
+          <li className="p-4 rounded-xl bg-[#dcedc1]">
+            <span className="text-irisBlueColor text-[16px] leading-6 font-semibold ">
+              {formatDate("01-23-2013")} - {formatDate("05-23-2018")}
+            </span>
+            <p className="text-[18px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
+            <p className="text-[16px] leading-5 font-medium text-textColor">
+              Korean Research Hospital, Seoul
+            </p>
+          </li>
+        </ul>
       </div>
     </div>
   );
--- FE_DocFinder/src/pages/doctors/DoctorDetail.jsx
+++ FE_DocFinder/src/pages/doctors/DoctorDetail.jsx
@@ -10,7 +10,7 @@ export const DoctorDetail = () => {
 
   return (
     <section>
-      <div className="max-w-[1170px] px-5 mx-auto">
+      <div className="max-w-[1200px] px-5 mx-auto">
         <div className="grid md:grid-cols-3 gap-[50px]">
           <div className="md:col-span-2 ">
             <div className="flex items-center gap-5">
