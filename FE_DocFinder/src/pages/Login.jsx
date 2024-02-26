d0ff8ba9aeca1ca88b1114765269aee9b3dd8cca
bransyahtan <bransyahhhh@gmail.com>
2024-02-26 20:08:13 +0700

    login page designing

--- FE_DocFinder/src/pages/Login.jsx
+++ FE_DocFinder/src/pages/Login.jsx
@@ -1,3 +1,14 @@
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className="px-5 lg:px-0">
+      <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
+        <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
+          Hello! <span className="text-primaryColor">Welcome</span> Back
+          &#127881;
+        </h3>
+
+        <form></form>
+      </div>
+    </section>
+  );
 };
