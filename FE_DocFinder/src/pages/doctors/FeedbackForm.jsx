66f15620100599fa9c59c16395481a8f31faeb1e
bransyahtan <bransyahhhh@gmail.com>
2024-02-26 19:18:17 +0700

    add star rating in doctor review

--- FE_DocFinder/src/pages/doctors/FeedbackForm.jsx
+++ FE_DocFinder/src/pages/doctors/FeedbackForm.jsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
+import { AiFillStar } from "react-icons/ai";
+
 export const FeedbackForm = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
   return (
-    <div>
-      <p>feedback Form</p>
-    </div>
+    <form action="">
+      <div>
+        <h3 className="text-headingColor text-[18px] leading-6 font-semibold mb-4">
+          How would you rate the overall experience?
+        </h3>
+
+        <div>
+          {[...Array(5).keys()].map((_, index) => {
+            index += 1;
+
+            return (
+              <button
+                key={index}
+                type="button"
+                className={`${
+                  index <= ((rating && hover) || hover)
+                    ? "text-yellowColor"
+                    : "text-gray-400"
+                } bg-transparent border-none outline-none text-[22px] cursor-pointer`}
+                onClick={() => setRating(index)}
+                onMouseEnter={() => setHover(index)}
+                onMouseLeave={() => setHover(rating)}
+                onDoubleClick={() => {
+                  setHover(0);
+                  setRating(0);
+                }}
+              >
+                <span>
+                  <AiFillStar />
+                </span>
+              </button>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="mt-7"></div>
+    </form>
   );
 };
