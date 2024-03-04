09465524b5a66f16d6ed1cd8d62ea2e0f0ebc47a
bransyahtan <bransyahhhh@gmail.com>
2024-03-04 16:15:49 +0700

    test be

--- BE_DocFinder/controllers/authControllers.js
+++ BE_DocFinder/controllers/authControllers.js
@@ -46,7 +46,7 @@ export const register = async (req, res) => {
     await user.save();
     res.status(200).json({
       success: true,
-      message: "User created successfully",
+      message: "User Created Successfully",
     });
   } catch (error) {
     res.status(500).json({
